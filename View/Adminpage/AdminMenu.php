3ab522a3c7a478877f532188bffd420396b057a1
Sathyabalan <sathyabalasundaran@diligentsquad.com>
2022-02-23 17:50:21 +0530

    Link Added

--- View/Adminpage/AdminMenu.php
+++ View/Adminpage/AdminMenu.php
@@ -1,9 +1,9 @@
 <div id="admin-nav">
-<div><a href="">Add new Snack</a></div>
-<div><a href="">Assign Snacks</a></div>
+<div><a href="/var/www/html/Snackapp/View/Snack/Newsnackinsert.php">Add new Snack</a></div>
+<div><a href="/var/www/html/Snackapp/View/Assignment/Snackassignment.php">Assign Snacks</a></div>
 <div><a href="">List Assigned Snacks</a></div>
-<div><a href="">Choose Snack For the Day</a></div>
-<div><a href="">Create Admin User</a></div>
+<div><a href="/var/www/html/Snackapp/View/Snack/ChooseSnack.php">Choose Snack For the Day</a></div>
+<div><a href="/var/www/html/Snackapp/View/Registration/AdminRegistration.php">Create Admin User</a></div>
 <div><a href=""></a></div>
 </br></br>
 </div>
